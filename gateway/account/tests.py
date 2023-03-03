4b10c552b185ce8d420896c72db89bc5bcad0338
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-03-03 13:26:56 +0100

    add some tests

--- gateway/account/tests.py
+++ gateway/account/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from account.models import Account, Subscriptions
+
+
+class TestAuthentication(APITestCase):
+
+    def setUp(self) -> None:
+        # arrange
+        self.user = User(email='test@test.com', password='testtest')
+        self.account = Account(user=self.user, subscription=Subscriptions.STANDARD)
+
+    def test_account_creation_failure(self):
+        # act
+        res = self.client.post('/api/user/', data={'email': self.user.email, 'password': 'test'})
+        # assert
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_account_creation_success(self):
+        # act
+        res = self.client.post('/api/user/', data={'email': self.user.email,
+                                                   'password': self.user.password})
+        # assert
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
