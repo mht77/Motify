78ae5f5c96430eaad98a243242906e079c442259
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-03-09 21:15:16 +0100

    use map for grpc response of accounts

--- gateway/grpc_services/account_service.py
+++ gateway/grpc_services/account_service.py
@@ -7,8 +7,8 @@ class AccountService(account_pb2_grpc.AccountService):
 
     def GetAccounts(self, request, context, **kwargs):
         query = Account.objects.filter(id__in=request.id)
-        res = [account_pb2.Account(id=account.id, username=account.user.username, email=account.user.email,
-                                   date_joined=str(account.user.date_joined),
-                                   subscription=account.subscription)
-               for account in query]
+        res = {account.id: account_pb2.Account(id=account.id, username=account.user.username, email=account.user.email,
+                                               date_joined=str(account.user.date_joined),
+                                               subscription=account.subscription)
+               for account in query}
         return account_pb2.GetAccountsResponse(accounts=res)
