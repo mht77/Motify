f3a246235fad40d005ae00c36631a8953c4eb64c
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-04-19 15:58:16 +0200

    better user_player model

--- music/user_player/admin.py
+++ music/user_player/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from user_player.models import Device, UserPlayer
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'user',
+        'ip',
+        'name',
+        'success'
+    ]
+
+
+@admin.register(UserPlayer)
+class UserPlayerAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'current_song',
+        'device',
+        'second',
+        'state'
+    ]
--- music/user_player/models.py
+++ music/user_player/models.py
@@ -1,20 +1,20 @@
 import uuid
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from song.models import Song
 
 
 class Device(models.Model):
     name = models.CharField(
-        max_length=20,
+        max_length=50,
         null=True,
         blank=True,
     )
 
     user = models.CharField(
-        max_length=40,
-        unique=True
+        max_length=40
     )
 
     ip = models.CharField(
@@ -23,8 +23,15 @@ class Device(models.Model):
         blank=True
     )
 
+    success = models.BooleanField(default=False)
+
     def __str__(self):
-        return self.user[:8] + self.name
+        return self.ip + '_' + self.name
+
+
+class PlayerState(models.TextChoices):
+    PLAYING = 'PLAYING', _('Playing')
+    PAUSED = 'PAUSED', _('Paused')
 
 
 class UserPlayer(models.Model):
@@ -44,6 +51,14 @@ class UserPlayer(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True
+    )
+
+    second = models.PositiveIntegerField(default=0)
+
+    state = models.CharField(
+        max_length=10,
+        choices=PlayerState.choices,
+        default=PlayerState.PAUSED
     )
 
     device = models.ForeignKey(
