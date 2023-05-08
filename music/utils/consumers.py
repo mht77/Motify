f867fc30a2193443a0aa4a11053284a11bcd3879
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-05-08 17:52:02 +0200

    remove user data across the app

--- music/utils/consumers.py
+++ music/utils/consumers.py
@@ -32,6 +32,7 @@ class RabbitListener(threading.Thread, abc.ABC):
         self.channel.basic_consume(queue=self.queue, on_message_callback=self.callback, auto_ack=True)
         self.channel.start_consuming()
 
+    @abc.abstractmethod
     def callback(self, ch, method, properties, body):
         pass
 
@@ -42,7 +43,6 @@ class UserCreatedListener(RabbitListener):
         super().__init__(queue='music_user', exchange='user-created', exchange_type='fanout')
 
     # noinspection PyUnusedLocal
-    # @staticmethod
     def callback(self, ch, method, properties, body):
         try:
             user_data = msgpack.unpackb(body)
@@ -58,7 +58,6 @@ class UserLoggedInListener(RabbitListener):
         super().__init__(queue='user_logged_in')
 
     # noinspection PyUnusedLocal
-    # @staticmethod
     def callback(self, ch, method, properties, body):
         try:
             device = msgpack.unpackb(body)
@@ -72,5 +71,21 @@ class UserLoggedInListener(RabbitListener):
             if user_player and not user_player.device:
                 user_player.device = entity
                 user_player.save()
+        except Exception as e:
+            logging.error(e)
+
+
+class UserDeleteListener(RabbitListener):
+
+    def __init__(self):
+        super().__init__(queue='music_user_delete', exchange='user-delete', exchange_type='fanout')
+
+    def callback(self, ch, method, properties, body):
+        try:
+            user_data = msgpack.unpackb(body)
+            logging.info(f'User Deleted: {user_data}')
+            UserPlayer.objects.filter(user=user_data['id']).delete()
+            for device in Device.objects.filter(user=user_data['id']):
+                device.delete()
         except Exception as e:
             logging.error(e)
