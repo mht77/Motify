653cc02ae5f333fb05ea4387d42adb5527edae40
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-06-11 18:57:48 +0200

    music container improvement

--- music/song/services.py
+++ music/song/services.py
@@ -16,3 +16,10 @@ class SongService(song_pb2_grpc.SongService):
                                     file=song.file.url)
                       for song in Song.objects.filter(artist__name__icontains=request.keyword)])
         return song_pb2.SearchResponse(songs=songs)
+
+    def GetSongs(self, request, target, **kwargs):
+        songs = [song_pb2.Song(id=song.id, name=song.name, artist=song.artist.name, genre=song.genre.name,
+                               album=song.album.name, date_added=song.date_added.strftime("%Y/%m/%d"),
+                               file=song.file.url)
+                 for song in Song.objects.filter(id__in=request.id)]
+        return song_pb2.SearchResponse(songs=songs)
