1f466c477eca97ea1f2323453e816f1e784f4a79
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-06-16 11:59:50 +0200

    fix a minor issue in websocket view

--- music/user_player/views.py
+++ music/user_player/views.py
@@ -15,7 +15,7 @@ class UserPlayerConsumer(JsonWebsocketConsumer):
         )
         self.accept()
         print('connected: ', self.scope['user'], self.scope['device'])
-        self.send_json({'message': 'connected'})
+        # self.send_json({'message': 'connected'})
         user_player = UserPlayer.objects.filter(user=self.scope['user']).first()
         cache.set(self.scope['user'], UserPlayerSerializer(user_player).data)
         self.send_json(UserPlayerSerializer(user_player).data)
@@ -33,6 +33,7 @@ class UserPlayerConsumer(JsonWebsocketConsumer):
                     pass
                 user_player.state = PlayerState.PAUSED
                 user_player.save()
+                cache.delete(self.scope['user'])
         except Device.DoesNotExist:
             pass
         except UserPlayer.DoesNotExist:
@@ -45,20 +46,25 @@ class UserPlayerConsumer(JsonWebsocketConsumer):
         Called with decoded JSON content.
         """
         cache_player = cache.get(self.scope['user'])
+        second = cache_player.get('second', 0)
         if content.get('song') or content.get('device') or content.get('state'):
             user_player = UserPlayer.objects.filter(user=self.scope['user']).first()
             try:
                 user_player.state = content.get('state', user_player.state)
                 if content.get('song'):
                     user_player.current_song_id = content['song']
+                    user_player.current_song.no_plays += 1
+                    user_player.current_song.save()
                 if content.get('device'):
                     user_player.device_id = content['device']
                 user_player.save()
                 cache_player = UserPlayerSerializer(user_player).data
+                cache_player['second'] = second
             except KeyError:
                 pass
         if content.get('second'):
             cache_player['second'] = content['second']
+        cache.set(self.scope['user'], cache_player)
         async_to_sync(self.channel_layer.group_send)(
             self.scope['user'], {'type': 'user_player_update', 'message': cache_player}
         )
